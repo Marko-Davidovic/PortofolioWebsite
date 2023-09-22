8f084968d1d62c692562e1a19bd14c49567f53e5
Marko Davidovic <davidovic-m@hotmail.com>
2023-09-22 13:31:19 -0400

    home page

---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.linkedin.com/in/marko-davidovic-209a601a3/"
+        className="home__social-icon"
+      >
+        <i className="uil uil-linkedin-alt"></i>
+      </a>
+
+      <a
+        href="https://github.com/Marko-Davidovic"
+        className="home__social-icon"
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+
+      <a
+        href="https://www.instagram.com/marko__davidovic/"
+        className="home__social-icon"
+      >
+        <i className="uil uil-instagram"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
