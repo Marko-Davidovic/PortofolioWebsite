0deaf74a6c1c6e6ec52ed045ef604fc2aab35b82
Marko Davidovic <davidovic-m@hotmail.com>
2023-09-26 14:02:15 -0400

    scorll Up and Active Links added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Home/Home";
 import Skills from "./components/Skills/Skills";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
+import ScrollUp from "./components/ScrollUp/ScrollUp";
 
 function App() {
   return (
@@ -16,8 +17,10 @@ function App() {
         <About />
         <Skills />
         <Contact />
-        <Footer />
+        
       </main>
+      <Footer />
+      <ScrollUp />
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,65 +1,99 @@
-import React, { useState } from 'react'
-import  "./header.css"
+import React, { useState } from "react";
+import "./header.css";
 
 const Header = () => {
+  //scrollUp
+  window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    if (this.scrollY >= 80) header.classList.add("scroll-header");
+    else header.classList.remove("scroll-header");
+  });
   //Toggle Menu
   const [Toggle, showMenu] = useState(false);
+  const [activeNav, setActiveNav] = useState("#about");
   return (
-    <header className='header' >
+    <header className="header">
       <nav className="nav container">
-        <a href="index.html" className="nav__logo">Davidovic</a>
-        <div className={Toggle ? "nav__menu show-menu" : "nav__menu"} >
-          <ul className='nav__list grid'>
-            <li className='nav__item'>
-              <a href="#home" className="nav__link active-link">
-                <i className='uli uli-estate nav__icon'>
-                  Home
-                </i>
+        <a href="index.html" className="nav__logo">
+          Davidovic
+        </a>
+        <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a
+                href="#home"
+                onClick={() => setActiveNav("#home")}
+                className={
+                  activeNav === "#home"
+                    ? "nav__link active-link"
+                    : "nav__link"
+                }
+              >
+                <i className="uli uli-estate nav__icon">Home</i>
               </a>
             </li>
 
-            <li className='nav__item'>
-              <a href="#about" className="nav__link">
-                <i className='uli uli-user nav__icon'>
-                  About
-                </i>
+            <li className="nav__item">
+              <a href="#about"
+               onClick={() => setActiveNav("#about")}
+               className={
+                 activeNav === "#about"
+                   ? "nav__link active-link"
+                   : "nav__link"
+               }
+                >
+                <i className="uli uli-user nav__icon">About</i>
               </a>
             </li>
 
-            <li className='nav__item'>
-              <a href="#skills" className="nav__link">
-                <i className='uli uli-file-alt nav__icon'>
-                  Skills
-                </i>
+            <li className="nav__item">
+              <a href="#skills" 
+               onClick={() => setActiveNav("#skills")}
+               className={
+                 activeNav === "#skills"
+                   ? "nav__link active-link"
+                   : "nav__link"
+               }>
+                <i className="uli uli-file-alt nav__icon">Skills</i>
               </a>
             </li>
 
-            <li className='nav__item'>
-              <a href="#portofolio" className="nav__link">
-                <i className='uli uli-scenery nav__icon'>
-                  Portofolio
-                </i>
+            <li className="nav__item">
+              <a href="#portofolio" 
+              onClick={() => setActiveNav("#portofolio")}
+               className={
+                 activeNav === "#portofolio"
+                   ? "nav__link active-link"
+                   : "nav__link"
+               }>
+                <i className="uli uli-scenery nav__icon">Portofolio</i>
               </a>
             </li>
 
-            <li className='nav__item'>
-              <a href="#contact" className="nav__link">
-                <i className='uli uli-message nav__icon'>
-                  Contact
-                </i>
+            <li className="nav__item">
+              <a href="#contact" 
+               onClick={() => setActiveNav("#contact")}
+               className={
+                 activeNav === "#contact"
+                   ? "nav__link active-link"
+                   : "nav__link"
+               }>
+                <i className="uli uli-message nav__icon">Contact</i>
               </a>
             </li>
           </ul>
 
-          <i className='uil uil-times nav__close' onClick={()=> showMenu(!Toggle)}></i>
-
+          <i
+            className="uil uil-times nav__close"
+            onClick={() => showMenu(!Toggle)}
+          ></i>
         </div>
-        <div className='nav__toggle' onClick={()=> showMenu(!Toggle)}>
-          <i className='uil uil-apps'></i>
+        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+          <i className="uil uil-apps"></i>
         </div>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
